b450151a3cf4e18cd63e800deeb571f5eee3d9a5
Julian Theis <julian.theis@posteo.de>
2020-12-06 12:33:57 +0100

    Override neovim config in separate file

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -70,6 +70,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    (import ./neovim.nix)
     bat
     binutils
     borgbackup
@@ -89,71 +90,6 @@
     jetbrains.idea-community
     jdk14
     kotlin
-    (neovim.override {
-      configure = {
-        customRC = ''
-	  set autochdir
-	  set breakindent
-	  set clipboard+=unnamedplus
-	  set cursorline
-	  set linebreak
-	  set mouse=a
-	  set nofoldenable
-	  set number
-	  set signcolumn=yes
-	  set smartcase
-
-	  set tabstop=8 expandtab
-	  set softtabstop=4 shiftwidth=4
-
-          set termguicolors
-          colorscheme NeoSolarized
-          
-          autocmd filetype html,css,javascript,xml,json,yaml,conf setlocal shiftwidth=2 softtabstop=2
-
-          let g:deoplete#enable_at_startup = 1
-          let g:LanguageClient_serverCommands = {
-                      \ 'python': ['pyls'],
-                      \ 'cpp': ['clangd'],
-                      \ 'rust': ['rust-analyzer'],
-                      \ 'javascript': ['typescript-language-server', '--stdio'] }
-
-	  let mapleader = "\<space>"
-	  let maplocalleader = "\<space>"
-          
-          nmap <leader>cc <Plug>(lcn-menu)
-          nmap <leader>ch <Plug>(lcn-hover)
-          nmap <leader>cR <Plug>(lcn-rename)
-          nmap <leader>cd <Plug>(lcn-definition)
-          nmap <leader>cr <Plug>(lcn-references)
-          nmap <leader>ci <Plug>(lcn-implementation)
-          nmap <leader>cF <Plug>(lcn-format)
-          nmap <leader>cn <Plug>(cln-diagnostics-next)
-          nmap <leader>cp <Plug>(cln-diagnostics-prev)
-          
-          nnoremap <leader>f :FZF<cr>
-          nnoremap <leader>b :Buffers<cr>
-          nnoremap <leader>g :GitFiles<cr>
-          
-          nnoremap <leader>nn :NERDTreeFocus<cr>
-          nnoremap <leader>nt :NERDTreeToggle<cr>
-        '';
-
-        packages.neovim-custom = with pkgs.vimPlugins; {
-          start = [
-            LanguageClient-neovim
-            NeoSolarized
-            deoplete-nvim
-            fzf-vim
-            nerdtree
-            todo-txt-vim
-            vim-cpp-enhanced-highlight
-            vim-nix
-            #vimtex
-          ];
-        };
-      };
-    })
     nodejs
     pass
     pinentry-gnome
