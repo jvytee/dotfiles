f519bd7ffdcaca2028d561f73113b5a74eaee79c
Julian Theis <julian.theis@posteo.de>
2021-04-17 16:33:53 +0200

    Remove custom faces section

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -12,10 +12,3 @@
    '(yaml-mode which-key virtualenvwrapper solarized-theme simple-modeline projectile nix-mode magit lsp-ui lsp-ivy go-mode flycheck evil counsel company better-defaults use-package))
  '(vc-follow-symlinks nil)
  '(x-underline-at-descent-line t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 89)) (:foreground "#839496" :background "#002b36")))))
