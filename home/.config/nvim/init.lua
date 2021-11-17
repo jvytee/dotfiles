fe9d67a4e241735461b53206eb089941a62a39ac
Julian Theis <julian.theis@posteo.de>
2021-11-17 21:56:45 +0100

    Fix neovim key bindings

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -74,26 +74,26 @@ capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 local keymap_opts = { noremap = true, silent = true }
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(keys, cmd)
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', keys, cmd, keymap_opts)
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', keys, '<cmd>' .. cmd .. '<cr>', keymap_opts)
   end
 
-  buf_set_keymap('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-  buf_set_keymap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-  buf_set_keymap('K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-  buf_set_keymap('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-  buf_set_keymap('<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-  buf_set_keymap('<localleader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>')
-  buf_set_keymap('<localleader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>')
-  buf_set_keymap('<localleader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
-  buf_set_keymap('<localleader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-  buf_set_keymap('<localleader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-  buf_set_keymap('<localleader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-  buf_set_keymap('gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-  buf_set_keymap('<localleader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
-  buf_set_keymap('[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-  buf_set_keymap(']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-  buf_set_keymap('<localleader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>')
-  buf_set_keymap('<localleader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+  buf_set_keymap('gD', 'lua vim.lsp.buf.declaration()')
+  buf_set_keymap('gd', 'lua vim.lsp.buf.definition()')
+  buf_set_keymap('K', 'lua vim.lsp.buf.hover()')
+  buf_set_keymap('gi', 'lua vim.lsp.buf.implementation()')
+  buf_set_keymap('<C-k>', 'lua vim.lsp.buf.signature_help()')
+  buf_set_keymap('<localleader>wa', 'lua vim.lsp.buf.add_workspace_folder()')
+  buf_set_keymap('<localleader>wr', 'lua vim.lsp.buf.remove_workspace_folder()')
+  buf_set_keymap('<localleader>wl', 'lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))')
+  buf_set_keymap('<localleader>D', 'lua vim.lsp.buf.type_definition()')
+  buf_set_keymap('<localleader>rn', 'lua vim.lsp.buf.rename()')
+  buf_set_keymap('<localleader>ca', 'lua vim.lsp.buf.code_action()')
+  buf_set_keymap('gr', 'lua vim.lsp.buf.references()')
+  buf_set_keymap('<localleader>e', 'lua vim.lsp.diagnostic.show_line_diagnostics()')
+  buf_set_keymap('[d', 'lua vim.lsp.diagnostic.goto_prev()')
+  buf_set_keymap(']d', 'lua vim.lsp.diagnostic.goto_next()')
+  buf_set_keymap('<localleader>q', 'lua vim.lsp.diagnostic.set_loclist()')
+  buf_set_keymap('<localleader>f', 'lua vim.lsp.buf.formatting()')
 end
 
 local options = {
@@ -127,7 +127,7 @@ nvim_ts.setup {
 
 -- Set keymaps
 local function set_keymap(keys, cmd)
-  vim.api.nvim_set_keymap('n', keys, '<cmd>' .. cmd .. 'cr', keymap_opts)
+  vim.api.nvim_set_keymap('n', keys, '<cmd>' .. cmd .. '<cr>', keymap_opts)
 end
 
 set_keymap('<leader>b', 'Buffers')
