715332ae7210398ee8d123c7b6b14007d4b605dc
Julian Theis <theis@next-kraftwerke.de>
2023-08-03 13:11:32 +0200

    Use python indentation settings for groovy

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -28,7 +28,7 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'python',
+  pattern = 'python,groovy',
   callback = function()
     vim.opt_local.shiftwidth = 4
     vim.opt_local.softtabstop = 4
