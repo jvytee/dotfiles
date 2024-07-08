faf7c32cd24df2f4285ccc9b516b8428dece5294
Julian Theis <julian.theis@posteo.de>
2024-07-08 22:52:28 +0200

    Switch to tokyonight theme

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -1,11 +1,12 @@
 local config_colorscheme = function()
-  vim.cmd "colorscheme gruvbox"
+  vim.cmd "colorscheme tokyonight-night"
 end
 
 return {
   "LnL7/vim-nix",
   "cespare/vim-toml",
   { "ellisonleao/gruvbox.nvim", priority = 1000, config = config_colorscheme },
+  { "folke/tokyonight.nvim", priority = 1000, config = config_colorscheme },
   "freitass/todo.txt-vim",
   "hrsh7th/vim-vsnip",
   "lervag/vimtex",
