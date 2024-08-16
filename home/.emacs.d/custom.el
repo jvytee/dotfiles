92615f9e451fcaec1d818d875b8c9c9bd1cbf0c2
jvytee <jvytee@posteo.org>
2024-08-16 16:30:01 +0200

    Set fonts for fixed & variable pitch

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -24,4 +24,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t (:family "Iosevka"))))
+ '(variable-pitch ((t (:family "Cantarell")))))
