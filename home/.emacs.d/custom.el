50f02d894f73133249831bc4ed52390d3d820a72
jvytee <jvytee@posteo.org>
2024-09-03 08:45:32 +0200

    Adjust fonts

--- home/.emacs.d/custom.el
+++ home/.emacs.d/custom.el
@@ -23,5 +23,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Iosevka"))))
- '(variable-pitch ((t (:family "Cantarell")))))
+ '(fixed-pitch ((t (:family "JetBrains Mono"))))
+ '(variable-pitch ((t (:family "Inter")))))
