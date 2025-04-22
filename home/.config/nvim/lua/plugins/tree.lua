9aa3ea722f1b6cdf330af16fb87e5de717e40af0
jvytee <jvytee@posteo.org>
2025-04-22 15:30:06 +0200

    Fix indentation

--- home/.config/nvim/lua/plugins/tree.lua
+++ home/.config/nvim/lua/plugins/tree.lua
@@ -1,23 +1,23 @@
 local config = function()
-  local tree = require("nvim-tree")
-  tree.setup {
-    renderer = {
-      add_trailing = true,
-      group_empty = true,
-      highlight_git = true,
-      highlight_opened_files = "all",
-    },
-    view = {
-      width = 32,
-    },
-  }
+    local tree = require("nvim-tree")
+    tree.setup {
+        renderer = {
+            add_trailing = true,
+            group_empty = true,
+            highlight_git = true,
+            highlight_opened_files = "all",
+        },
+        view = {
+            width = 32,
+        },
+    }
 
-  local opts = require("on_attach").opts
-  vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeFocus<cr>", opts)
+    local opts = require("on_attach").opts
+    vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeFocus<cr>", opts)
 end
 
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = config
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = config
 }
