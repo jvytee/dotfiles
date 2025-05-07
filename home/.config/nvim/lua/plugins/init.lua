4870430c678944d9b33cefb00e2f97d7839a5cff
jvytee <jvytee@posteo.org>
2025-05-07 21:03:19 +0200

    Remove unused themes from neovim

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -6,7 +6,6 @@ return {
     "LnL7/vim-nix",
     "cespare/vim-toml",
     { "ellisonleao/gruvbox.nvim", priority = 1000, config = config_colorscheme },
-    { "miikanissi/modus-themes.nvim", priority = 1000, config = config_colorscheme },
     "hrsh7th/vim-vsnip",
     "lervag/vimtex",
 }
