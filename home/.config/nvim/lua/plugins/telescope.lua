cfa3d1137060f3905662e102bec90c7055790066
jvytee <jvytee@posteo.org>
2025-05-14 16:48:42 +0200

    Add a "command palette" shortcut

--- home/.config/nvim/lua/plugins/telescope.lua
+++ home/.config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,31 @@
 local config = function()
-  local builtin = require("telescope.builtin")
+    local builtin = require("telescope.builtin")
 
-  vim.keymap.set("n", "<leader>b", builtin.buffers, {})
-  vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-  vim.keymap.set("n", "<leader>g", builtin.git_files, {})
-  vim.keymap.set("n", "<leader>k", builtin.keymaps, {})
-  vim.keymap.set("n", "<leader>r", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>c", builtin.commands, {})
+    vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>g", builtin.git_files, {})
+    vim.keymap.set("n", "<leader>k", builtin.keymaps, {})
+    vim.keymap.set("n", "<leader>r", builtin.live_grep, {})
+
+    vim.keymap.set("n", "<localleader>e", function()
+        vim.diagnostic.open_float {
+            border = "rounded",
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            focusable = false,
+            scope = "cursor",
+            source = "always",
+        }
+    end, bufopts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, bufopts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, bufopts)
+    vim.keymap.set("n", "<localleader>q", builtin.diagnostics, bufopts)
 end
 
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  config = config,
-  dependencies = { "nvim-lua/plenary.nvim" },
-  priority = 900
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    config = config,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    priority = 900
 }
