b8301fd23bcfdc924ea1662f26c0c05efdfd79c2
jvytee <jvytee@posteo.org>
2025-06-04 10:11:24 +0200

    Add treesitter support for jinja

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,57 +1,59 @@
 local build = function()
-  local ts_update = require("nvim-treesitter.install").update { with_sync = true }
-  ts_update()
+    local ts_update = require("nvim-treesitter.install").update { with_sync = true }
+    ts_update()
 end
 
 local config = function()
-  local nvim_ts = require("nvim-treesitter.configs")
-  nvim_ts.setup {
-    ensure_installed = {
-      "c",
-      "cpp",
-      "css",
-      "dockerfile",
-      "glsl",
-      "go",
-      "gomod",
-      "gosum",
-      "groovy",
-      "haskell",
-      "hcl",
-      "html",
-      "htmldjango",
-      "java",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "meson",
-      "nginx",
-      "nix",
-      "python",
-      "rust",
-      "scheme",
-      "terraform",
-      "toml",
-      "typescript",
-      "tsx",
-      "xml",
-      "yaml",
-    },
+    local nvim_ts = require("nvim-treesitter.configs")
+    nvim_ts.setup {
+        ensure_installed = {
+            "c",
+            "cpp",
+            "css",
+            "dockerfile",
+            "glsl",
+            "go",
+            "gomod",
+            "gosum",
+            "groovy",
+            "haskell",
+            "hcl",
+            "html",
+            "htmldjango",
+            "java",
+            "javascript",
+            "jinja",
+            "jinja_inline",
+            "json",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "meson",
+            "nginx",
+            "nix",
+            "python",
+            "rust",
+            "scheme",
+            "terraform",
+            "toml",
+            "typescript",
+            "tsx",
+            "xml",
+            "yaml",
+        },
 
-    highlight = { enable = true },
-    indent = { enable = true },
-    refactor = {
-      highlight_definitions = {
-        enable = true,
-        clear_on_cursor_move = true,
-      },
-    },
-  }
+        highlight = { enable = true },
+        indent = { enable = true },
+        refactor = {
+            highlight_definitions = {
+                enable = true,
+                clear_on_cursor_move = true,
+            },
+        },
+    }
 end
 
 return {
-  { "nvim-treesitter/nvim-treesitter", build = build, config = config },
-  "nvim-treesitter/nvim-treesitter-refactor"
+    { "nvim-treesitter/nvim-treesitter", build = build, config = config },
+    "nvim-treesitter/nvim-treesitter-refactor"
 }
