bf3ce5a5b4ea1292b8016b5a7a43c2256f113dde
jvytee <jvytee@posteo.org>
2025-07-03 18:22:22 +0200

    Let's tokyonight for a while

--- home/.config/nvim/lua/plugins/misc.lua
+++ home/.config/nvim/lua/plugins/misc.lua
@@ -1,11 +1,12 @@
 local config_colorscheme = function()
-    vim.cmd "colorscheme gruvbox"
+    vim.cmd "colorscheme tokyonight"
 end
 
 return {
     "LnL7/vim-nix",
     "cespare/vim-toml",
     { "ellisonleao/gruvbox.nvim", priority = 1000, config = config_colorscheme },
+    { "folke/tokyonight.nvim", priority = 1000, config = config_colorscheme },
     "lervag/vimtex",
     "mfussenegger/nvim-ansible",
 }
