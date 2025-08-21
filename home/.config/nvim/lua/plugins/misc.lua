49509e1e4bdf1ce8038d5bdb62e50b25a05436d3
jvytee <jvytee@posteo.org>
2025-08-21 10:48:35 +0200

    Return to tokyonight-moon theme for better contrast

--- home/.config/nvim/lua/plugins/misc.lua
+++ home/.config/nvim/lua/plugins/misc.lua
@@ -1,13 +1,13 @@
 local config_colorscheme = function()
-    vim.cmd "colorscheme onedark"
+    vim.cmd "colorscheme tokyonight"
 end
 
 return {
     "LnL7/vim-nix",
     "cespare/vim-toml",
-    { "ellisonleao/gruvbox.nvim", priority = 1000, config = config_colorscheme },
+    { "ellisonleao/gruvbox.nvim", priority = 1000 },
     { "folke/tokyonight.nvim", priority = 1000, config = config_colorscheme },
     "lervag/vimtex",
     "mfussenegger/nvim-ansible",
-    { "navarasu/onedark.nvim", priority = 1000, config = config_colorscheme },
+    { "navarasu/onedark.nvim", priority = 1000 },
 }
