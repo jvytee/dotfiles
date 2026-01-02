56f89668fbf99fb022d128542f609aeeb327bc58
jvytee <jvytee@posteo.org>
2026-01-02 16:04:03 +0100

    Add shortcut to debug python tests

--- home/.config/nvim/lua/plugins/dap.lua
+++ home/.config/nvim/lua/plugins/dap.lua
@@ -16,6 +16,8 @@ local config = function()
     local dap_python = require "dap-python"
     dap_python.setup "debugpy-adapter"
     dap_python.test_runner = "pytest"
+    vim.keymap.set("n", "<localleader>dt", dap_python.test_method)
+    vim.keymap.set("n", "<localleader>dT", dap_python.test_class)
 end
 
 return {
