c7a4ac3ce665d31286c1c40e42ba2f900fa6cbe4
jvytee <jvytee@posteo.org>
2026-01-19 18:37:20 +0100

    Fix vimtex config

--- home/.config/nvim/lua/plugins/misc.lua
+++ home/.config/nvim/lua/plugins/misc.lua
@@ -7,6 +7,6 @@ return {
     { "LnL7/vim-nix", ft = "nix" },
     { "cespare/vim-toml", ft = "toml" },
     { "folke/tokyonight.nvim", priority = 1000, config = config_colorscheme },
-    { "lervag/vimtex", ft = "tex" },
+    { "lervag/vimtex", lazy = false },
     { "mfussenegger/nvim-ansible", ft = "yaml" },
 }
