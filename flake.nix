ebc0d6e158f6e633443af99e6814f92b5a838327
jvytee <jvytee@posteo.org>
2026-04-12 19:54:45 +0200

    Add dev shell for deployment tools

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Personal dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default =
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          opentofu
+          tofu-ls
+        ];
+      };
+  };
+}
