24c26bdf9fd9d8f0a8c7364cbd17915418efe1e7
jvytee <jvytee@posteo.org>
2026-04-14 21:59:00 +0200

    Autoformat flake

--- flake.nix
+++ flake.nix
@@ -5,14 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          opentofu
-          tofu-ls
-        ];
-      };
-  };
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShells.x86_64-linux.default =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            opentofu
+            tofu-ls
+          ];
+        };
+    };
 }
