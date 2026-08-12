5625b2291ceec1b2aed1e9741ba1d54bb6e5b207
jvytee <jvytee@posteo.org>
2026-08-12 15:00:34 +0200

    Try filtering carriage returns during zig compilation

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -364,4 +364,7 @@
 
 (use-package zig-mode
   :mode ("\\.zig\\'" "\\.zon\\'")
-  :hook (compilation-filter . ansi-color-compilation-filter))
+  :hook
+  ((compilation-filter . ansi-color-compilation-filter)
+   (compilation-filter . (lambda ()
+                           (replace-string-in-region "\r" "\n" compilation-filter-start (point))))))
